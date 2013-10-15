efcd34d5b07431bffdd7c0e83d559571010460f4
Bruno Humic <bruno.humic@fer.hr>
2013-10-15 19:12:29 +0200

    Some warnings fixed

--- AnomalyDetector/src/anomalyDetector/featureExtraction/TotalEntitiesExtractor.java
+++ AnomalyDetector/src/anomalyDetector/featureExtraction/TotalEntitiesExtractor.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import android.app.ActivityManager;
+//import android.app.ActivityManager;
 
 /**
  * 
