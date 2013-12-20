30dbb851de3c5884bb841db4efb1c7fa17693597
Bruno Humic <bruno.humic@fer.hr>
2013-12-20 11:32:11 +0100

    Support for dynamic graph ploting added

--- AnomalyDetector/src/anomalyDetector/services/ColectFeaturesService.java
+++ AnomalyDetector/src/anomalyDetector/services/ColectFeaturesService.java
@@ -1,20 +1,19 @@
 package anomalyDetector.services;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.achartengine.GraphicalView;
 
-
-
+import android.annotation.SuppressLint;
 //import android.app.ActivityManager;
 import android.app.Service;
 import android.content.Context;
@@ -24,15 +23,18 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 import anomalyDetector.activities.MainFeaturesActivity;
 import anomalyDetector.factories.FeatureExtractorFactory;
 import anomalyDetector.featureExtraction.FeatureExtractor;
+import anomalyDetector.graph.AnonPagesGraph;
 
 /**
  * 
  * @author Bruno Humic
  * @License GPLv3
  */
+@SuppressLint("SimpleDateFormat")
 public class ColectFeaturesService extends Service {
 
 	private FeatureExtractorFactory factory;
@@ -40,15 +42,28 @@ public class ColectFeaturesService extends Service {
 	private ScheduledExecutorService scheduleTaskExecutor;
 	private ArrayList<FeatureExtractor> featureExtractors;
 	
+	public static String externalStorageDirName = "collected_features";
+	//private FileOutputStream fOutputStream;
 	private String fileName = "collected_features";
-	private FileOutputStream fOutputStream;
-	
-	private String externalFileName = "external_collected_features";
+	private String filePath = "collectedFeatureStorage";
 	private FileOutputStream externalFileOutputStream;
-	private File externalDirectory;
 	private File externalFile;
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd");
+	private int elapsedTime = 0;
+	/*
+	 * Reference to graph object
+	 */
+	private static AnonPagesGraph lineGraph;
 	
 	public static final String COLECT_FEATURE_SERVICE = "anomalyDetector.services.ColectFeaturesService";
+	
+	
+	public class LocalBinder extends Binder{
+		ColectFeaturesService getService(){
+			return ColectFeaturesService.this;
+		}
+	}
+	private final IBinder mBinder = new LocalBinder();
 	
 	@Override
 	public void onCreate() {
@@ -56,22 +71,19 @@ public class ColectFeaturesService extends Service {
 		factory = new FeatureExtractorFactory();
 		scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
 		featureExtractors = new ArrayList<FeatureExtractor>();
+		lineGraph = new AnonPagesGraph("Anonymous pages");
 		
 		/*
 		 * Directory in external memory where the data is stored. 
 		 * A file in that directory where the data is stored
 		 */
-		externalDirectory = getFileStorageDir(getApplicationContext(), fileName);
-		externalFile = new File(externalDirectory.getAbsolutePath(), externalFileName);
+		//fileName += dateFormat.format(new Date());
 		
-		try {
-			fOutputStream = openFileOutput(fileName, Context.MODE_APPEND);
-			/*
-			 * File output stream to the file in external storage
-			 */
-			externalFileOutputStream = new FileOutputStream(externalFile);
-		} catch (FileNotFoundException e) {
-			Log.d("Fos error", "Error opening file output stream");
+		if(isExternalStorageWritable()){
+			externalFile = new File(getExternalFilesDir(filePath), fileName);
+		}
+		else{
+			Log.d("Service:EXTERNAL_STORAGE", "External storage is not writable");
 		}
 	}
 	
@@ -82,6 +94,27 @@ public class ColectFeaturesService extends Service {
 		features = bundle.getStringArrayList(MainFeaturesActivity.SELECTED_FEATURES);
 		
 		createExtractors();
+		String fstLine = "";
+		for(FeatureExtractor fExtractor : featureExtractors){
+			fstLine += fExtractor.getName() + ",";
+		}
+		
+		try {
+			if(externalFile.exists()){
+				//Log.d("POKRENUT", String.valueOf(externalFile.exists()));
+				externalFileOutputStream = new FileOutputStream(externalFile, true);
+			}
+			else{
+				externalFileOutputStream = new FileOutputStream(externalFile, true);
+				fstLine += "\n";
+				writeToExternalStorage(fstLine);
+			}
+			/*
+			 * File output stream to the file in external storage
+			 */
+		} catch (FileNotFoundException e) {
+			Log.d("Fos error", "Error opening file output stream for external storage");
+		}
 		
 		scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
 			
@@ -108,21 +141,19 @@ public class ColectFeaturesService extends Service {
 		Log.d("STOP", "Service stoped");
 		scheduleTaskExecutor.shutdown();
 		try {
-			fOutputStream.close();
+			//fOutputStream.close();
 			externalFileOutputStream.close();
 			//fInputStream.close();
 		} catch (IOException e) {
 			Log.d("Fos error", "Error closing file output stream");
 		}
-		// TODO Auto-generated method stub
 		super.onDestroy();
 	}
 	
 	
 	@Override
 	public IBinder onBind(Intent arg0) {
-		//TODO
-		return null;
+		return mBinder;
 	}
 	
 	private void createExtractors(){
@@ -135,29 +166,23 @@ public class ColectFeaturesService extends Service {
 	
 	private void getData(){
 		
-		StringBuffer buffer = new StringBuffer();
+		StringBuffer buffer = new StringBuffer();		
 		
 		for(FeatureExtractor f : featureExtractors){
-			Log.d(f.getName(), String.valueOf(f.extract()));
+			//Log.d(f.getName(), String.valueOf(f.extract()));
 			buffer.append(f.extract() + " ");
 		}
 
 		buffer.append("\n");
 		String line = buffer.toString();
+		elapsedTime += 2;
+		lineGraph.addNewPoint(line, elapsedTime);
 		
 		if(!isExternalStorageWritable()){
 			Log.d("External storage INFO", "External storage IS NOT writable");
 		}
 		else{
 			writeToExternalStorage(line);
-		}
-		
-		
-		try {
-			fOutputStream.write(line.getBytes());
-			fOutputStream.flush();
-		} catch (IOException e) {
-			Log.d("Fos error", "Error while writing data to file output stream");
 		}
 	}
 	
@@ -183,7 +208,7 @@ public class ColectFeaturesService extends Service {
 	 * Checks if external storage is available for read and write
 	 * @return True if external storage is available, false otherwise
 	 */
-	public boolean isExternalStorageWritable(){
+	public static boolean isExternalStorageWritable(){
 		String state = Environment.getExternalStorageState();
 		if(Environment.MEDIA_MOUNTED.equals(state)){
 			return true;
@@ -195,7 +220,7 @@ public class ColectFeaturesService extends Service {
 	 * Checks if external storage is available to at least reading
 	 * @return True if external storage is at least readable, false otherwise
 	 */
-	public boolean isExternalStorageReadable(){
+	public static boolean isExternalStorageReadable(){
 		String state = Environment.getExternalStorageState();
 		if(Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)){
 			return true;
@@ -204,17 +229,23 @@ public class ColectFeaturesService extends Service {
 	}
 	
 	/**
-	 * Directory where the data will be stored
+	 * Create the directory structure on external storage
+	 * where the data will be saved.
 	 * @param context Application context
-	 * @param fileName name of file to store the data
+	 * @param directoryName name of file to store the data
 	 * @return File reference to where the data will be stored
 	 */
-	public File getFileStorageDir(Context context, String fileName){
+	public static File getFileStorageDir(Context context, String directoryName){
 		
-		File file = new File(context.getExternalFilesDir(null), fileName);
+		File file = new File(context.getExternalFilesDir(null), directoryName);
 		if(!file.mkdirs()){
 			Log.e("External storage error", "Directory not created");
 		}
+		
 		return file;
+	}
+	
+	public static AnonPagesGraph getLineGraph() {
+		return lineGraph;
 	}
 }
