1514a4bab5ed60a466a2528e614c63662507838c
Bruno Humic <bruno.humic@fer.hr>
2014-03-24 22:00:26 +0100

    A new class which implements the on item click listener for items in navigation drawer.

---
+++ AnomalyDetector/src/anomalyDetector/utils/DrawerItemClickListener.java
@@ -0,0 +1,43 @@
+package anomalyDetector.utils;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import anomalyDetector.activities.ShowAnonPagesActivity;
+
+public class DrawerItemClickListener implements ListView.OnItemClickListener{
+
+	private final String TAG = "DRAWER_ITEM_CLICK_LISTENER";
+	private boolean isServiceRunning = false;
+	
+	public DrawerItemClickListener() {
+	}
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		
+		/*
+		 * If we selected to view the graphs
+		 */
+		if(position == 0){
+			if(!isServiceRunning){
+				Toast toast = Toast.makeText(parent.getContext(), "Service not running", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+			else{
+				Intent intent = new Intent(parent.getContext(), ShowAnonPagesActivity.class);
+				parent.getContext().startActivity(intent);
+			}
+		}
+		else{
+			//TODO
+		}
+	}
+	
+	public void setServiceRunning(boolean isServiceRunning) {
+		this.isServiceRunning = isServiceRunning;
+	}
+
+}
